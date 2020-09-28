466eee6b985a17c23846afc1c4e4468ca1f10bb8
zhangboyi <15813380401@163.com>
2020-09-28 16:06:19 +0800

    feat: kyi

--- src/main/java/com/itheima/HelloServlet.java
+++ src/main/java/com/itheima/HelloServlet.java
@@ -14,13 +14,13 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req,resp);
+        super.doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //模拟代码错误
-        int i = 100/0;
+        int i = 100 / 0;
 
         //模拟冗余代码
         int a = 100;
---
+++ src/main/java/com/test/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import com.itheima.HelloWorld;
+//import com.sun.tools.javac.util.Assert;
+
+public class HelloWorldTest {
+    @Test
+    public void testMethod1() {
+        HelloWorld hw = new HelloWorld();
+        String ss = hw.testMethod1();
+        Assert.assertNotNull(ss);
+    }
+
+    @Test
+    public void testMethod2() {
+        HelloWorld hw = new HelloWorld();
+        int ss = hw.addMethod(1, 1);
+        Assert.assertEquals(ss, 2);
+    }
+}
